151cbec2cf94dd022c89a9b09a9e24c4f11c6c6e
Code Sandbox <hello@codesandbox.io>
2022-07-16 19:04:53 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.ae493270303220a536af33d3d1d95dea.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b7de71f3c9b26d9f7ef",
+    "url": "https://tshoko.github.io/csb-9vhwxo/static/css/main.2a61c5de.chunk.css"
+  },
+  {
+    "revision": "8b7de71f3c9b26d9f7ef",
+    "url": "https://tshoko.github.io/csb-9vhwxo/static/js/main.f798c784.chunk.js"
+  },
+  {
+    "revision": "942822fa883f6b3c5ed6",
+    "url": "https://tshoko.github.io/csb-9vhwxo/static/js/runtime~main.56197934.js"
+  },
+  {
+    "revision": "da8d5d8d7a080a34e2c9",
+    "url": "https://tshoko.github.io/csb-9vhwxo/static/js/2.ac4a04eb.chunk.js"
+  },
+  {
+    "revision": "53534c648d125827dc3e",
+    "url": "https://tshoko.github.io/csb-9vhwxo/static/js/3.3c918dd4.chunk.js"
+  },
+  {
+    "revision": "c3d56406d132fab97642d06d39b9eab3",
+    "url": "https://tshoko.github.io/csb-9vhwxo/static/media/about.c3d56406.jpg"
+  },
+  {
+    "revision": "635d509a026c29e3a9c9a5e71370981c",
+    "url": "https://tshoko.github.io/csb-9vhwxo/static/media/certificate1.635d509a.png"
+  },
+  {
+    "revision": "6eec8eca38d0692c03f81a3d6ae4f9d7",
+    "url": "https://tshoko.github.io/csb-9vhwxo/static/media/Akanyang.6eec8eca.jpg"
+  },
+  {
+    "revision": "03dc027d412e75abb2b2ae3746bf5d96",
+    "url": "https://tshoko.github.io/csb-9vhwxo/static/media/hero.03dc027d.jpg"
+  },
+  {
+    "revision": "50ede55535ae0e013ac574d7a4959779",
+    "url": "https://tshoko.github.io/csb-9vhwxo/index.html"
+  }
+];
